e26a1c5953c5af41356d350ae568d7018fca537c
Alok Singh <65681896+Alok-Singh-ops@users.noreply.github.com>
2025-04-04 16:39:44 +0530

    new changes done

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:5173", credentials: true })); // Allow frontend
-// app.use(v1Router)
+// app.use(v1Router) chages
 
 app.use(
   session({
