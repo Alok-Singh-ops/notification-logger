d39615728345af9b35679a6882e7967a8dfeac20
Alok Singh <65681896+Alok-Singh-ops@users.noreply.github.com>
2025-04-05 23:07:36 +0530

    new chanegs

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:5173", credentials: true })); // Allow frontend
-// app.use(v1Router) chages dghdhhd
+// app.use(v1Router) chages dghdhhdfdjdhfh
 
 app.use(
   session({
